e01b3969709347b2bd0c2858dae9dc44c648d8ab
Anand Lilhare <anandlilhare69@gmail.com>
2017-05-24 09:11:15 +0530

    add basic storyboard and navigation controler

---
+++ RecipeBook/RecipeBook/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RecipeBook
+//
+//  Created by Anand Lilhare on 24/05/17.
+//  Copyright © 2017 Anand Lilhare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
---
+++ RecipeBook/RecipeBook/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  RecipeBook
+//
+//  Created by Anand Lilhare on 24/05/17.
+//  Copyright © 2017 Anand Lilhare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+{
+  NSArray *recipes;
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    // Initialize table data
+    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"RecipeCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
+}
+@end
